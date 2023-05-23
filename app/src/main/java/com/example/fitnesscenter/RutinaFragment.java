3a04d3510e93d3b109530161c29ff18383ae98ad
cesargc826 <cesardaniel826@gmail.com>
2023-05-22 18:41:15 -0600

    Actualizacion del fragment rutina y las 5 imagenes del fragment rutina

--- app/src/main/java/com/example/fitnesscenter/RutinaFragment.java
+++ app/src/main/java/com/example/fitnesscenter/RutinaFragment.java
@@ -40,19 +40,59 @@ public class RutinaFragment extends Fragment {
         addInventario = (FloatingActionButton) root.findViewById(R.id.BtnAgregar);
         items = new ArrayList<>();
 
-        items.add(new ItemList("Lunes", "", R.drawable.psychotic_red));
-        items.add(new ItemList("Martes", "18", R.drawable.psychotic_black));
-        items.add(new ItemList("Miercoles", "25", R.drawable.psychotic));
-        items.add(new ItemList("Jueves", "l. Jalón abierto al frente en triserie con remo sentado y" +
-                "expansión de dorsales 4 de 12, \n" +
-                "2. Remo con mancuerna a 2 manos en triserie con peso muerto " +
-                "y jalona al frente con triangulo 4 de 12, \n" +
-                "3. Jalón nuca en biserie con buenos días 4 de 12, \n" +
-                "4. Jalón con cuerda en biserie con copa a dos manos 4 de 12, \n" +
-                "5, Jalón con cuerda barra corta en triserie con press francés y " +
-                "press california 4 de 12 y " +
-                "6. Fondos en biserie con patada de mula 4 de 12.", R.drawable.optimun_nutrition));
-        items.add(new ItemList("Viernes", "30", R.drawable.pw_nitraflex));
+        items.add(new ItemList("Pierna", "Calentamiento 10-15 minutos\n" +
+                "\n" +
+                "Sentadilla libre 4/12-10-8-failure\n" +
+                "Femoral acostado 4/12-15 (ya sabes la velocidad)\n" +
+                "\n" +
+                "Goblet squat en disco con desplante 4/12 por cada pie.\n" +
+                "Desplantes parado 4/ 15 por cada pie\n" +
+                "\n" +
+                "Sumo squat 4/12-10-8-8\n" +
+                "Leg extension 4/12-8 (Drop sets)\n" +
+                "\n" +
+                "Pantorrilla en máquina 4/25-30", R.drawable.pierna_rutina));
+        items.add(new ItemList("Espalda", "Calenamiento 10 - 15 minutos\n" +
+                "\n" +
+                "Remo con barra 4/8-10 \n" +
+                "Pullover con soga 4/12-15 \n" +
+                "\n" +
+                "Remo neutro en maquina 4/8-10\n" +
+                "Pulldown prono 4/10\n" +
+                "\n" +
+                "Remo con mancuernas 4/10\n" +
+                "Pulldown neutro 4/10", R.drawable.espalda_rutina));
+        items.add(new ItemList("Brazo", "Calenamiento 10 - 15 minutos\n" +
+                "\n" +
+                "Curl de bicep 4/8-12\n" +
+                "Dips 4/failure\n" +
+                "\n" +
+                "Martillos 4/8-10\n" +
+                "Extensiones en soga 4/10\n" +
+                "\n" +
+                "Predicador 4/8-12\n" +
+                "Kickback 4/10-12", R.drawable.brazo_rutina));
+        items.add(new ItemList("Hombro", "Calenamiento 10 - 15 minutos\n" +
+                "\n" +
+                "Press miltar 4/8-10\n" +
+                "Facepull 4/12-15\n" +
+                "\n" +
+                "Laterales 4/10-12\n" +
+                "Frontales 4/10-12\n" +
+                "\n" +
+                "Elevacion de hombros 4/20", R.drawable.hombro_rutina));
+        items.add(new ItemList("Gluteo", "Calentamiento 10-15 minutos\n" +
+                "\n" +
+                "Sentadilla libre 4/8-12\n" +
+                "Abductor 4/15\n" +
+                "\n" +
+                "Leg extension 2/12-15 y 2/drop sets\n" +
+                "Hip thrust 4/8-12 con 3s en la última isometrica\n" +
+                "\n" +
+                "Femoral sentado o acostado 4/12-15\n" +
+                "Patada en cables (si se puede en el banco) 4/12 LENTAS\n" +
+                "\n" +
+                "Pantorrilla 4/20-25", R.drawable.gluteo_rutina));
 
         LinearLayoutManager manager = new LinearLayoutManager(getContext());
         rvLista.setLayoutManager(manager);
